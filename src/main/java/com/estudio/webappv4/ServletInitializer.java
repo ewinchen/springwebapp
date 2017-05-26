c6fecfd1b6b564ea94a7a170f9b141642afa6f06
Ewin <ewinchen@163.com>
2017-05-27 02:16:31 +0800

    Initialization,初始化工程目录

---
+++ src/main/java/com/estudio/webappv4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.estudio.webappv4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Webappv4Application.class);
+	}
+
+}
---
+++ src/main/java/com/estudio/webappv4/Webappv4Application.java
@@ -0,0 +1,12 @@
+package com.estudio.webappv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webappv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webappv4Application.class, args);
+	}
+}
